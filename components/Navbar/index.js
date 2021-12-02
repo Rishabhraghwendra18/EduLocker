1e10b1ab6ff509f016969d55d844694a6961c68b
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-12-02 16:48:21 +0530

    Home page done without navBar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
-import { Navbar, Container ,Nav, NavDropdown } from "react-bootstrap";
-import logo from '../../public/logo.png'
+import { Navbar, Container ,Nav} from "react-bootstrap";
+import styles from '../../styles/Navbar.module.css'
+import logo from '../../public/logo.svg'
 
 export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Image src={logo} width={80} height={80}></Image>
+        <Image src={logo} className={`${styles.navbar}`}></Image>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className='mx-auto'>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import banner from '../public/Banner.svg'
+import deal from '../public/deal.png'
+import fileLogo from '../public/file.png'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div className={`${styles.container} d-flex flex-column align-items-stretch vh-100`}>
+      <Image src={banner}></Image>
+      <div className={`p-3 ${styles.features_div}`}>
+        <div>
+        <h3 style={{textAlign:'center',marginBottom:'0 !important'}}>Features</h3>
+        </div>
+        <div className={`d-flex flex-row ${styles.features}`}>
+          <div className={`handShake d-flex flex-column justify-content-center align-items-center p-2 ${styles.handShake_div}`}>
+          <Image src={deal} width={100} height={100}></Image>
+          <h4>Pay school/college fees as per the law</h4>
+          <p>No need of paying high fees pay as per smart contract</p>
+          </div>
+          <div className="document d-flex flex-column justify-content-center align-items-center p-2">
+          <Image src={fileLogo} width={100} height={100}></Image>
+          <h4>All your docs are safe with us</h4>
+          <p>No need of paying high fees pay as per smart contract</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
